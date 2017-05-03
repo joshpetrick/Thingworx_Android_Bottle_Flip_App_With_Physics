9449daef70f46a35014bb25b327cab4c9c77cae3
cdavis <cdavis@ptc.com>
2017-05-03 15:47:36 -0400

    Connectivity App Updated

---
+++ examples/src/main/java/com/mbientlab/bletoolbox/examples/ScannerActivity.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2015 MbientLab Inc. All rights reserved.
+ */
+
+package com.mbientlab.bletoolbox.examples;
+
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.mbientlab.bletoolbox.scanner.BleScannerFragment;
+
+import java.util.UUID;
+
+public class ScannerActivity extends AppCompatActivity implements BleScannerFragment.ScannerCommunicationBus {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_scanner, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDeviceSelected(BluetoothDevice device) {
+        Intent intent = this.getIntent();
+        intent.putExtra(MainScanActivity.EXTRA_BLE_DEVICE, device);
+        this.setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    @Override
+    public UUID[] getFilterServiceUuids() {
+        return new UUID[] {UUID.fromString("326a9000-85cb-9195-d9dd-464cfbbae75a")};
+    }
+
+    @Override
+    public long getScanDuration() {
+        return 10000;
+    }
+}
